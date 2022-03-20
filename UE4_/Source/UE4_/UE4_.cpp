5eebc54bc904d37b6a09bd3869f9beeddfced2e2
hwankee <abcy100@nate.com>
2022-03-21 06:22:23 +0900

    init

--- UE4_/Source/UE4_/UE4_.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE4_.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_, "UE4_" );
- 
