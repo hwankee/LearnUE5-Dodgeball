babd2e86e467a88828562078ab351250789d700c
hwankee <abcy100@nate.com>
2022-05-02 21:10:28 +0900

    🍍 wall

--- Dodgeball/Intermediate/Build/Mac/x86_64/UnrealEditor/Inc/Dodgeball/Dodgeball.init.gen.cpp
+++ Dodgeball/Intermediate/Build/Mac/x86_64/UnrealEditor/Inc/Dodgeball/Dodgeball.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeDodgeball_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB1142740,
-				0xD6B4622B,
+				0x4487C44E,
+				0xD8A69806,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Dodgeball.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Dodgeball.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Dodgeball(Z_Construct_UPackage__Script_Dodgeball, TEXT("/Script/Dodgeball"), Z_Registration_Info_UPackage__Script_Dodgeball, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB1142740, 0xD6B4622B));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Dodgeball(Z_Construct_UPackage__Script_Dodgeball, TEXT("/Script/Dodgeball"), Z_Registration_Info_UPackage__Script_Dodgeball, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x4487C44E, 0xD8A69806));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Dodgeball/Intermediate/Build/Mac/x86_64/UnrealEditor/Inc/Dodgeball/VictoryBox.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DODGEBALL_VictoryBox_generated_h
+#error "VictoryBox.generated.h already included, missing '#pragma once' in VictoryBox.h"
+#endif
+#define DODGEBALL_VictoryBox_generated_h
+
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_SPARSE_DATA
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_RPC_WRAPPERS
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVictoryBox(); \
+	friend struct Z_Construct_UClass_AVictoryBox_Statics; \
+public: \
+	DECLARE_CLASS(AVictoryBox, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Dodgeball"), NO_API) \
+	DECLARE_SERIALIZER(AVictoryBox)
+
+
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAVictoryBox(); \
+	friend struct Z_Construct_UClass_AVictoryBox_Statics; \
+public: \
+	DECLARE_CLASS(AVictoryBox, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Dodgeball"), NO_API) \
+	DECLARE_SERIALIZER(AVictoryBox)
+
+
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVictoryBox(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVictoryBox) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVictoryBox); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVictoryBox); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVictoryBox(AVictoryBox&&); \
+	NO_API AVictoryBox(const AVictoryBox&); \
+public:
+
+
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVictoryBox(AVictoryBox&&); \
+	NO_API AVictoryBox(const AVictoryBox&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVictoryBox); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVictoryBox); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AVictoryBox)
+
+
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_9_PROLOG
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_SPARSE_DATA \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_RPC_WRAPPERS \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_INCLASS \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_SPARSE_DATA \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Dodgeball_Source_Dodgeball_VictoryBox_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DODGEBALL_API UClass* StaticClass<class AVictoryBox>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Dodgeball_Source_Dodgeball_VictoryBox_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
